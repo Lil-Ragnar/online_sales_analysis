462276c027091dc69ca98e2bb5af7070836285c1
Brabete Mihai Marian <brabetemihai1@gmail.com>
2025-03-12 16:52:29 +0100

    Made a few changes in main.

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from product_manager import Product_manager
 
 manager = Product_manager
 
-manager.add_product(Product("Placa Video", 5000, 10))
-manager.add_product(Product("Tableta", 1400, 24))
-manager.add_product(Product("Iphone", 1200, 15))
+manager.add_product(Product("Placa Video", 5000, 13))
+manager.add_product(Product("Tableta", 1400, 14))
+manager.add_product(Product("Iphone", 1200, 3))
 
-manager.display_products()
-
-manager.calculate_total_value()
